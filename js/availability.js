6e5e51f339c83bf73b33e19ee0b7a6f2b1c72081
Alison Mohr <alison@newbo.co>
2020-06-17 15:19:40 -0500

    debugged js. updated availability

--- js/availability.js
+++ js/availability.js
@@ -5,7 +5,7 @@ console.log('This file is connected');
 //Greet the user using prompt
 var userName = prompt ('What is your name?');
 console.log(userName);
-alert('Hello,' + userName + '! Welcome to Six Sigma Properties.');
+alert('Hello,' + ' ' + userName + '! Welcome to Six Sigma Properties.');
 
 //prompt our user for five yes or no questions
 var bedroomCount = prompt('How many bedrooms would you like?');
@@ -14,9 +14,9 @@ console.log(bedroomCount);
 if(bedroomCount === '3' || bedroomCount === '2') {
   console.log('We do have units available for rent that should meet your needs.');
   alert('We do have units available for rent that should meet your needs.');
-} else if(bedroomCount === '1'|| lowerCaseFood === '4') {
+} else if(bedroomCount === '1'|| bedroomCount === '4') {
   console.log ('We do not have any available units at this time.');
-  alert('Incorrect, I love ice cream.');
+  alert('We do not have any available units at this time.');
 }
 else {
   console.log ('The maximum number of bedrooms we have is 4. Please input your selection using the numbers 1,2,3, or 4.');
@@ -26,7 +26,7 @@ else {
 var apartmentRequest = prompt('Please add any other requests that you would help us find you the perfect place.');
 console.log(apartmentRequest);
 
-var userEmail = prompt('Thank you for that information' + userName + '. Please include type your email and one of our agents will get back to you soon.');
+var userEmail = prompt('Thank you for that information' + ' '+ userName + '. Please include type your email and one of our agents will get back to you soon.');
 console.log(userEmail);
 
-alert('Thank you! Have a nice day,' + userName +'!');
+alert('Thank you! Have a nice day,'+ ' ' + userName +'!');
